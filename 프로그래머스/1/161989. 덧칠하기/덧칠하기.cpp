3cdc855b9ca9fb9467b8ff68198eba3de4fc53b4
gradeten <88281367+gradeten@users.noreply.github.com>
2024-03-15 22:30:42 +0900

    [level 1] Title: 덧칠하기, Time: 0.23 ms, Memory: 4.91 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int current = 0;
+    
+    for(int i = 0; i < section.size(); i++){
+        if (current > section[i]){
+            continue;
+        }
+        answer++;
+        current = section[i] + m;
+    }
+    
+    return answer;
+}
