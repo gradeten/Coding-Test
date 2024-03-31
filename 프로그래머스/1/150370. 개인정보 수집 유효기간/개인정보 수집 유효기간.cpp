5202e7b0cc49fdc2e900656870dda4c76569e3b6
gradeten <88281367+gradeten@users.noreply.github.com>
2024-03-31 15:07:25 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.06 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -11,49 +11,15 @@ vector<int> solution(string today, vector<string> terms, vector<string> privacie
             if (privacies[i][11] == terms[j][0]){
                 
                 int l = stoi(terms[j].substr(2));
-                vector<string> date(3);
-                int cnt = 0;
                 
-                for(int k = 0; k < 10; k++){
-                    if (privacies[i][k] != '.'){
-                        date[cnt] += privacies[i][k];
-                    }
-                    else{
-                        cnt++;
-                    }
-                }
-                if (date[1][0] == '0'){
-                    date[1].erase(0,1);
-                }
-                if (date[2][0] == '0'){
-                    date[2].erase(0,1);
-                }
+                int y = stoi(privacies[i].substr(0,4));
+                int m = stoi(privacies[i].substr(5,2));
+                int d = stoi(privacies[i].substr(8,2));
                 
-                int y = stoi(date[0]);
-                int m = stoi(date[1]);
-                int d = stoi(date[2]);
-                
-                vector<string> t(3);
-                cnt = 0;
-                
-                for(int k = 0; k < 10; k++){
-                    if (today[k] != '.'){
-                        t[cnt] += today[k];
-                    }
-                    else{
-                        cnt++;
-                    }
-                }
-                if (t[1][0] == '0'){
-                    t[1].erase(0,1);
-                }
-                if (t[2][0] == '0'){
-                    t[2].erase(0,1);
-                }
-                
-                int t_y = stoi(t[0]);
-                int t_m = stoi(t[1]);
-                int t_d = stoi(t[2]);
+            
+                int t_y = stoi(today.substr(0,4));
+                int t_m = stoi(today.substr(5,2));
+                int t_d = stoi(today.substr(8,2));
                 
                 if ((y*28*12 + m*28 + d + l*28) <= (t_y*28*12 + t_m*28 + t_d)){
                     answer.push_back(i+1);
