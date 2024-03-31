c4acfab713705831a284b30f719edcbbb8544eaf
gradeten <88281367+gradeten@users.noreply.github.com>
2024-03-31 15:10:00 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -4,14 +4,5 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 0;
-    
-    if (s[0] == '-'){
-        s.erase(0,1);
-        return (0-stoi(s));
-    }
-    else{
-        return stoi(s);
-    }
-
+    return(stoi(s));
 }
