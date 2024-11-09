232690f51af76f47370473149ee037e2b2e7b062
gradeten <88281367+gradeten@users.noreply.github.com>
2024-11-09 19:03:25 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -1,29 +1,19 @@
 #include <iostream>
+#include <algorithm>  
 using namespace std;
 
-int main(){
-    int a, b, c, g;
+int main() {
+    int a, b, c;
     cin >> a >> b >> c;
-    
-    if ((a == b) && (b == c))
-    {
+
+    if (a == b && b == c) {
         cout << 10000 + a * 1000;
     }
-    else if (a == b)
-    {
-        cout << 1000 + a * 100;
+    else if (a == b || b == c || a == c) {
+        int same = (a == b) ? a : ((b == c) ? b : c);
+        cout << 1000 + same * 100;
     }
-    else if (b == c)
-    {
-        cout << 1000 + b * 100;
-    }
-    else if (a == c)
-    {
-        cout << 1000 + a * 100;
-    }
-    else
-    {
-        g = max(a, max(b, c));
-        cout << g * 100;
+    else {
+        cout << max({a, b, c}) * 100;
     }
 }
