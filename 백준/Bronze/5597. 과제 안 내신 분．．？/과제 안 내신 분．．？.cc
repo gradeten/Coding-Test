62ad3fd796b75382ffaf6b5a56270475fb9bd8e8
gradeten <88281367+gradeten@users.noreply.github.com>
2024-12-04 22:03:39 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int a;
+    vector<int> v(30,0);
+    
+    for(int i = 0; i < 28; i++)
+    {
+        cin >> a;
+        v[a-1] = a;
+    }
+    
+    for(int i = 0; i < 30; i++)
+    {
+        if (v[i] == 0)
+        {
+            cout << i + 1 << endl;
+        }
+    }
+}
