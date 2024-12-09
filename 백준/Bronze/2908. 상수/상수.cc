42fb4e11409ed8170207bdb71b60230bf8ada4c2
gradeten <88281367+gradeten@users.noreply.github.com>
2024-12-09 22:49:22 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    
+    if (stoi(s1) > stoi(s2))
+    {
+        cout << stoi(s1);
+    }
+    else
+    {
+        cout << stoi(s2);
+    }
+}
