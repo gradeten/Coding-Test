2bb053677ce05e2bf4b934551a1412e7eae0dfd7
gradeten <88281367+gradeten@users.noreply.github.com>
2024-12-13 20:23:47 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -6,22 +6,15 @@ int main()
 {
     vector<int> v(6);
     vector<int> answer = {1,1,2,2,2,8};
-    int a;
     
     for(int i = 0; i < 6; i++)
     {
-        cin >> a;
-        v[i] = a;
+        cin >> v[i];
     }
     
     for(int i = 0; i < 6; i++)
     {
-        v[i] = answer[i] - v[i];
-    }
-    
-    for(int i = 0; i < 6; i++)
-    {
-        cout << v[i] << " ";
+        cout << answer[i] - v[i] << " ";
     }
     
 }
