3fb788f5dc8b872b20c090f5fd5e9bf848faf1ff
gradeten <88281367+gradeten@users.noreply.github.com>
2024-12-14 19:17:43 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void printStar(int i, int n)
+{
+    for(int j = 1; j <= (2*n - 1 - i)/2; j++)
+        {
+            cout << " ";
+        }
+        for(int j = 0; j < i; j++) cout << "*";
+        cout << endl;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if (n == 1)
+    {
+        cout << "*";
+    }
+    else
+    {
+        for(int i = 1; i <= 2*n-1; i+=2)
+        {
+            printStar(i, n);
+        }
+    
+        for(int i = 2*n-3; i >= 1; i-=2)
+        {
+            printStar(i, n);
+        }
+    }
+    
+}
+
+
