198d8b33f40705aecbb9db3a0e7bf02be0dae3e8
gradeten <88281367+gradeten@users.noreply.github.com>
2025-01-13 07:48:41 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a;
+    int min = 100;
+    int sum = 0;
+    
+    for (int i = 0; i < 7; i++)
+    {
+        cin >> a;
+        if (a % 2 != 0)
+        {
+            if (a < min) min = a;
+            sum += a;
+        }
+    }
+    
+    if (sum == 0)
+    {
+        cout << -1;
+    }
+    else
+    {
+        cout << sum << "\n" << min;
+    }
+}
