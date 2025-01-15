c6e632172723c39193f3a65ed0e785d27cbefce0
gradeten <88281367+gradeten@users.noreply.github.com>
2025-01-16 08:18:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    for(int i = N; i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
