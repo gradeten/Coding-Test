b7c24350ca6683d6ba4cd067ac5b3efcdff1c464
gradeten <88281367+gradeten@users.noreply.github.com>
2025-01-15 10:26:02 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N, n;
+    cin >> N;
+    int y = 0, m = 0;
+    
+    for(int i = 0; i < N; i++)
+    {
+        cin >> n;
+        y += (n/30+1)*10;
+        m += (n/60+1)*15;
+    }
+    
+    if (y < m)
+    {
+        cout << "Y " << y; 
+    }
+    else if (m < y)
+    {
+        cout << "M " << m; 
+    }
+    else
+    {
+        cout << "Y M " << y; 
+    }
+}
