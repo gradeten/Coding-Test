127f57cdeec9354263a723cb8817fea8ded0c1f1
gradeten <88281367+gradeten@users.noreply.github.com>
2025-01-19 08:02:55 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int num[10] = {0};
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    unsigned int result = A * B * C;
+    string s = to_string(result);
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        num[s[i]-'0']++;
+    }
+    
+    for(int i = 0; i < 10; i++)
+    {
+        cout << num[i] << " ";
+    }
+    
+}
