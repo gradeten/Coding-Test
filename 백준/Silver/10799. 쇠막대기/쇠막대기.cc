8f28dbd844b569d1d95f42596d24e655cfcce3d6
gradeten <88281367+gradeten@users.noreply.github.com>
2025-02-02 20:49:49 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string s;
+    cin >> s;
+    stack<char> S;
+    int answer = 0;
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            S.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (!S.empty() && S.top() == '(')
+            {
+                S.pop();
+                if (s[i-2] == '(' && s[i-1] == ')')
+                {
+                    answer++;
+                    // cout << answer << "\n";
+                }
+                else
+                {
+                    if (s[i-1] == ')') answer++;
+                    else answer += S.size();
+                    // cout << answer << "\n";
+                }
+            }
+        }
+    }
+    
+    cout << answer;
+}
