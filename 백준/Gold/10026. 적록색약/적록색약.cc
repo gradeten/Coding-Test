d6444ca41ac0c96be21986eead510f590442ce1d
gradeten <88281367+gradeten@users.noreply.github.com>
2025-02-10 07:32:55 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -2,74 +2,62 @@
 using namespace std;
 #define X first
 #define Y second
+char board[101][101];
+bool vis[101][101];
+int n;
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
-bool blind[101][101] = {false};
-bool color[101][101] = {false};
+
+void bfs(int i, int j){
+    queue<pair<int,int>> Q;
+    Q.push({i,j});
+    while (!Q.empty()){
+        auto cur = Q.front(); Q.pop();
+        for (int dir = 0; dir < 4; dir++){
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+            if (vis[nx][ny] == 1 || board[i][j] != board[nx][ny]) continue;
+            vis[nx][ny] = 1;
+            Q.push({nx,ny});
+        }
+    }
+}
+
+int area(){
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (!vis[i][j]){
+                cnt++;
+                bfs(i,j);
+            }
+        }
+    }
+    return cnt;
+}
 
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int N;
-    cin >> N;
-    string board[N];
-    for(int i = 0; i < N; i++) cin >> board[i];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> board[i][j];
     
-    queue<pair<int,int> > Q;
+    int not_g = area();
     
-    int area = 0;
-    for (char c : {'R', 'G', 'B'}){
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                if (board[i][j] == c && !color[i][j]){
-                    area++;
-                    color[i][j] = 1;
-                    Q.push({i,j});
-                    while(!Q.empty()){
-                        pair<int,int> cur = Q.front(); Q.pop();
-                        for(int dir = 0; dir < 4; dir++){
-                            int nx = cur.X + dx[dir];
-                            int ny = cur.Y + dy[dir];
-                            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                            if (board[nx][ny] != c || color[nx][ny]) continue;
-                            color[nx][ny] = 1;
-                            Q.push({nx,ny});
-                        }
-                    }
-                }
-            }
-        }
-    }
+    for (int i = 0; i < n; i++) fill (vis[i], vis[i]+n, false);
     
-    cout << area << " ";
-    for(int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
             if (board[i][j] == 'G') board[i][j] = 'R';
-    
-    area = 0;
-    for (char c : {'R', 'B'}){
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
-                if (board[i][j] == c && !blind[i][j]){
-                    area++;
-                    blind[i][j] = 1;
-                    Q.push({i,j});
-                    while(!Q.empty()){
-                        pair<int,int> cur = Q.front(); Q.pop();
-                        for(int dir = 0; dir < 4; dir++){
-                            int nx = cur.X + dx[dir];
-                            int ny = cur.Y + dy[dir];
-                            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                            if (board[nx][ny] != c || blind[nx][ny]) continue;
-                            blind[nx][ny] = 1;
-                            Q.push({nx,ny});
-                        }
-                    }
-                }
-            }
         }
     }
-    cout << area << " ";
+    
+    int is_g = area();
+    cout << not_g << " " << is_g;
+    return 0;
     
 }
