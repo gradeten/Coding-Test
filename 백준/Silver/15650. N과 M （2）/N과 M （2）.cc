0f01b0a77ea86c2c460084ce17ebea77c05400ae
gradeten <88281367+gradeten@users.noreply.github.com>
2025-03-02 09:58:11 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    int arr[n];
+    
+    for(int i = 0; i < n; i++) (i < m) ? arr[i] = 0 : arr[i] = 1;
+    
+    do{
+        for(int i = 0; i < n; i++){
+            if (arr[i] == 0) cout << i + 1 << ' ';
+        }
+        cout << '\n';
+    }while (next_permutation(arr,arr+n));
+    
+}
