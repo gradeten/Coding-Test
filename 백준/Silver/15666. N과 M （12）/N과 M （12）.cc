bc68a748e2bc11eaa3b12c5f20c2a31e49141355
gradeten <88281367+gradeten@users.noreply.github.com>
2025-03-11 11:01:54 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int arr[10];
+int ans[10];
+
+void func(int k, int st){
+    if (k == m){
+        for(int i = 0; i < m; i++) cout << ans[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int prev = -1;
+    for(int i = st; i < n; i++){
+        if (prev != arr[i]){
+            ans[k] = arr[i];
+            prev = arr[i];
+            func(k+1,i);
+        }
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr,arr+n);
+    
+    func(0,0);
+}
