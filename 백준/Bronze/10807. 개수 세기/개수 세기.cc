4cbba47ad20fb68b62b7cc46c8573b8d0fea29c3
gradeten <88281367+gradeten@users.noreply.github.com>
2025-03-15 16:45:59 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,30 +1,21 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int n, a, t;
-    vector<int> v;
-    int answer = 0;
+int main (void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
+    int n, a;
     cin >> n;
-    for(int i = 0; i < n; i++)
-    {
-        cin >> a;
-        v.push_back(a);
+    int arr[n];
+    
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    cin >> a;
+    
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        if (arr[i] == a) ans++;
     }
     
-    cin >> t;
-    
-    for(int i = 0; i < n; i++)
-    {
-        if (v[i] == t)
-        {
-            answer++;
-        }
-    }
-    
-    cout << answer;
+    cout << ans;
 }
