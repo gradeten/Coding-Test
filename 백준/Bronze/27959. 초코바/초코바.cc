14020d672b5217b1756ec2fdabb059d77306913e
gradeten <88281367+gradeten@users.noreply.github.com>
2025-03-20 16:09:09 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    if (100*n >= m) cout << "Yes";
+    else cout << "No";
+}
