6f4c5099d5979a8d3add4c7f715a6392d01d644e
gradeten <88281367+gradeten@users.noreply.github.com>
2025-03-24 16:02:09 +0900

    [Bronze IV] Title: Betting, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24751. Betting/Betting.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a;
+    cin >> a;
+    
+    cout << fixed << setprecision(10) << 100.0/a << '\n';
+    cout << fixed << setprecision(10) << 100.0/(100-a) << '\n';
+}
