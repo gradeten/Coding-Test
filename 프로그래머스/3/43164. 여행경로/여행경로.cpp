3c63553b20fb1e3ff3302475ca77dac9d4d0bdab
gradeten <88281367+gradeten@users.noreply.github.com>
2025-05-08 22:40:07 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> answer;
 bool finished = false;
+vector<string> answer;
 
-void dfs(string curr, vector<vector<string>>& tickets, vector<bool>& visited, vector<string>& path, int depth){
-    path.push_back(curr);
+void dfs(string curr, vector<vector<string>>& tickets, vector<bool>& visited, vector<string>& route, int depth){
+    route.push_back(curr);
     
     if (depth == tickets.size()){
-        answer = path;
+        answer = route;
         finished = true;
         return;
     }
-    
     for(int i = 0; i < tickets.size(); ++i){
         if (!visited[i] && tickets[i][0] == curr){
             visited[i] = true;
-            dfs(tickets[i][1], tickets, visited, path, depth + 1);
+            dfs(tickets[i][1], tickets, visited, route, depth + 1);
             if (finished) return;
             visited[i] = false;
         }
     }
     
-    path.pop_back();
+    route.pop_back();
 }
+
 
 vector<string> solution(vector<vector<string>> tickets) {
     sort(tickets.begin(), tickets.end());
-    
     vector<bool> visited(tickets.size(), false);
-    vector<string> path;
+    vector<string> route;
     
-    dfs("ICN", tickets, visited, path, 0);
+    dfs("ICN", tickets, visited, route, 0);
     return answer;
 }
