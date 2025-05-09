3193f399ec4effc03faaac5718ee8afc0fe56b83
gradeten <88281367+gradeten@users.noreply.github.com>
2025-05-09 14:22:04 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.10 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -2,31 +2,29 @@
 using namespace std;
 
 int solution(vector<vector<int>> jobs) {
-    int answer = 0, time = 0, idx = 0;
-    int count = 0;
+    int answer = 0, time = 0, idx = 0, count = 0;
     int n = jobs.size();
-
+    
     sort(jobs.begin(), jobs.end());
+    
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-
-    while (count < n) {
-        
-        while (idx < n && jobs[idx][0] <= time) {
-            pq.push({jobs[idx][1], jobs[idx][0]}); // {작업시간, 요청시간}
+    while(count < n){
+        while(idx < n && jobs[idx][0] <= time){
+            pq.push({jobs[idx][1], jobs[idx][0]});
             idx++;
         }
-
-        if (!pq.empty()) {
+        
+        if (!pq.empty()){
             auto [dur, start] = pq.top(); pq.pop();
             time += dur;
-            answer += time - start;
+            answer += (time - start);
             count++;
-        } else {
-            // 대기 중인 작업이 없으면 시간 한 칸 이동
+        }
+        else{
             time = jobs[idx][0];
         }
     }
-
+    
     return answer / n;
 }
