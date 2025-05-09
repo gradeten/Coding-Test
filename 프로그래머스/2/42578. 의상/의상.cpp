e2f60f08c5bf689b04c2fc87b6fa1502c81b98e4
gradeten <88281367+gradeten@users.noreply.github.com>
2025-05-09 10:24:27 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,25 +1,14 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    
-    map <string, int> m;
+    unordered_map<string, int> m;
     for(int i = 0; i < clothes.size(); i++){
-        string s = clothes[i][1];
-        if (m.find(s) != m.end()){
-            m[s]++;
-        }
-        else{
-            m.insert({s,1});
-        }
+        m[clothes[i][1]]++;
     }
-    
-    for(auto p : m) {
-        int i = (p.second+1);
-        answer *= i;
+    for (auto p : m){
+        answer *= (p.second+1);
     }
-    
     return answer-1;
 }
