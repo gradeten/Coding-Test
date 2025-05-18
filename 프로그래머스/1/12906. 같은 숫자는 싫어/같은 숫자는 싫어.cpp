74a22f5149d6db244b349b84c2f0d6864ee4f6c5
gradeten <88281367+gradeten@users.noreply.github.com>
2025-05-18 20:37:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 121.44 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<int> solution(vector<int> arr) 
 {
     vector<int> answer;
-    answer.push_back(arr[0]);
+    if (!arr.empty()) answer.push_back(arr[0]);
 
     for(int i = 1; i < arr.size(); i++){
         if (arr[i-1] != arr[i]){
