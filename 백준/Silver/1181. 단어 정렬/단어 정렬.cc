6bc01cb258b16597826dec72120d7f089eb000c8
gradeten <88281367+gradeten@users.noreply.github.com>
2025-06-09 22:38:40 +0900

    [Silver V] Title: 단어 정렬, Time: 16 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -2,28 +2,20 @@
 using namespace std;
 
 bool cmp(const string& a, const string& b){
-    if (a.size() != b.size()){
-        return a.size() < b.size();
-    }
-    else{
-        return a < b;
-    }
+    if (a.size() != b.size()) return a.size() < b.size();
+    return a < b;
 }
 
 int main (){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n;
-    cin >> n;
-    vector<string> v;
-    string s;
-    while(n--){
-        cin >> s;
-        if (find(v.begin(), v.end(), s) == v.end()) v.push_back(s);
-    }
+    int n; cin >> n;
+    vector<string> v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
     
     sort(v.begin(), v.end(), cmp);
+    v.erase(unique(v.begin(), v.end()), v.end());
     for (auto s : v) cout << s << "\n";
     return 0;
 }
