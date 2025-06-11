4f183bab0918af8aa645c472feec0f6d22834f05
gradeten <88281367+gradeten@users.noreply.github.com>
2025-06-11 13:08:00 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int d[1000000];
+int d[20];
 
 int main (){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    d[1] = 1; d[2] = 2; d[3] = 4;
+    for(int i = 4; i <= 11; i++) d[i] = d[i-1] + d[i-2] + d[i-3];
+
     int t, n;
     cin >> t;
-    d[1] = 1; d[2] = 2; d[3] = 4;
     while(t--){
         cin >> n;
-        for(int i = 4; i <= n; i++){
-            d[i] = d[i-1] + d[i-2] + d[i-3];
-        }
         cout << d[n] << "\n";
     }
 }
