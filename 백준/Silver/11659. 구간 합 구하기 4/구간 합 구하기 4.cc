35cf53aadc8aa30f1331f9ea9dfa63e89f732395
gradeten <88281367+gradeten@users.noreply.github.com>
2025-06-15 20:38:38 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -7,22 +7,16 @@ int main (){
     
     int n, m;
     cin >> n >> m;
-    int arr[n+1];
-    int d[n+1];
+    vector<int> prefix(n+1);
 
-    for(int i = 1; i <= n; i++){
-        cin >> arr[i];
-    }
-    
-    d[0] = 0;
-    d[1] = arr[1];
-    for(int i = 2; i <= n; i++){
-        d[i] = d[i-1] + arr[i];
+    for(int i = 1; i <= n; ++i){
+        int x; cin >> x;
+        prefix[i] = prefix[i-1] + x;
     }
     
     while(m--){
-        int start, end;
-        cin >> start >> end;
-        cout << d[end] - d[start-1] << "\n";
+        int a,b;
+        cin >> a >> b;
+        cout << prefix[b] - prefix[a-1] << "\n";
     }
 }
