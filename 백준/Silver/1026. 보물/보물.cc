ca1014ee6a342689abe91dc97679b95266d361a8
gradeten <88281367+gradeten@users.noreply.github.com>
2025-06-26 11:33:30 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
+int a[55];
+int b[55];
+int ans = 0;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n;
-    vector<int> a;
-    vector<int> b;
-    
     cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
     
-    for(int i = 0; i < n; i++){
-        int t; cin >> t;
-        a.push_back(t);
-    }
+    sort(a,a+n);
+    sort(b,b+n,greater<int>());
     
-    for(int i = 0; i < n; i++){
-        int t; cin >> t;
-        b.push_back(t);
-    }
+    for(int i = 0; i < n; i++) ans += a[i]*b[i];
     
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end(), greater<int>());
-    
-    int sum = 0;
-    for(int i = 0; i < n; i++){
-        sum += (a[i]*b[i]);
-    }
-    cout << sum;
+    cout << ans;
 }
