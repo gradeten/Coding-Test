db9a3f237433c4262e2631b5c3900eece339aedf
gradeten <88281367+gradeten@users.noreply.github.com>
2025-06-28 19:11:21 +0900

    [Gold III] Title: 공주님의 정원, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -8,34 +8,31 @@ int main(){
     cin.tie(0);
     
     cin >> n;
-    while(n--){
+    for(int i = 0; i < n; i++){
         int m1, d1, m2, d2;
         cin >> m1 >> d1 >> m2 >> d2;
-        flower.push_back({m1 * 100 + d1, m2 * 100 + d2});
+        flower.push_back({m1*100+d1, m2*100+d2});
     }
     
     sort(flower.begin(), flower.end());
     
     int current = 301;
-    int ans = 0, idx = 0, max_end = 0;
+    int idx = 0;
+    int ans = 0;
     
     while(current <= 1130){
-        bool found = false;
-        while(idx < flower.size() && flower[idx].first <= current){
-            if (flower[idx].second > max_end){
-                max_end = flower[idx].second;
-                found = true;
-            }
+        int next_end = current;
+        while(idx < n && flower[idx].first <= current){
+            next_end = max(next_end, flower[idx].second);
             idx++;
         }
-        if (!found){
+        if (next_end == current){
             cout << 0;
             return 0;
         }
-        current = max_end;
+        current = next_end;
         ans++;
     }
-    
-    
     cout << ans;
+    return 0;
 }
