6f71142ef1a4380191cc4abce35ff7be3d24638a
gradeten <88281367+gradeten@users.noreply.github.com>
2025-07-03 20:35:19 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -6,12 +6,13 @@ int main()
     int n;
     cin >> n;
     
-    for(int i = 2; i <=n; i++)
+    for(int i = 2; i*i <=n; i++)
     {
-        while (n % i == 0 && n != 1)
+        while (n % i == 0)
         {
-            cout << i << endl;
+            cout << i << '\n';
             n /= i;
         }
     }
+    if (n != 1) cout << n;
 }
