b0a3fd3c84eda7216987f7a1795f9e4927cc702e
gradeten <88281367+gradeten@users.noreply.github.com>
2025-07-08 11:24:49 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -6,13 +6,11 @@ int main(){
     cin.tie(0);
     
     int n; cin >> n;
-    int cnt = 0;
+    int ans = 0;
     
-    for(int i = 5; i <= n; i*= 5){
-        int q = n/i;
-        if (q == 0) break;
-        else cnt += q;
+    while(n){
+        ans += n/5;
+        n /= 5;
     }
-    
-    cout << cnt;
+    cout << ans;
 }
