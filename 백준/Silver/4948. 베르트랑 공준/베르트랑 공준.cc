3fa5fdd2789538fc7ae999183f46c765732f3a37
gradeten <88281367+gradeten@users.noreply.github.com>
2025-07-08 11:04:20 +0900

    [Silver II] Title: 베르트랑 공준, Time: 8 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<bool> state(247000, true);
-
-
+const int m = 123456 * 2;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n;
     
+    vector<bool> state(m + 1, true);
+    
     state[1] = false;
-    for(int i = 2; i <= 246912; i++){
+    for(int i = 2; i * i <= m; i++){
         if (!state[i]) continue;
-        for(int j = i * 2; j <= 246912; j += i){
-            if (!state[j]) continue;
+        for(int j = i * i; j <= m; j += i){
             state[j] = false;
         }
     }
