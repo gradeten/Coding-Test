0357880c96fba705f91fff37a04038aed1760070
gradeten <88281367+gradeten@users.noreply.github.com>
2025-07-10 16:11:33 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--){
+        int x, y;
+        cin >> x >> y;
+        
+        int dist = y - x;
+        int n = sqrt(dist);
+        
+        if (dist == n * n){
+            cout << 2 * n - 1 << '\n';
+        }
+        else if (dist <= n * n + n){
+            cout << 2 * n << '\n';
+        }
+        else{
+            cout << 2 * n + 1 << '\n';
+        }
+    }
+}
