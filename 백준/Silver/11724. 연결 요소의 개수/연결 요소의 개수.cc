bd65091285dfb0070cd49deabbdd03a24b63da34
gradeten <88281367+gradeten@users.noreply.github.com>
2025-09-01 19:58:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 72 ms, Memory: 6536 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, u, v;
+vector<int> adj[1005];
+bool vis[1005];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+   
+    cin >> n >> m;
+    while(m--){
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    int num = 0;
+    for(int i = 1; i <= n; i++){
+        if(vis[i]) continue;
+        num++;
+        queue<int> q;
+        q.push(i);
+        vis[i] = true;
+        while(!q.empty()){
+            int cur = q.front();
+            q.pop();
+            for(auto nxt : adj[cur]){
+                if(vis[nxt]) continue;
+                q.push(nxt);
+                vis[nxt] = true;
+            }
+        }
+    }
+    cout << num;
+}
