a3475ec3c0c76ab402be6fe5f6b43b641434c638
gradeten <88281367+gradeten@users.noreply.github.com>
2025-09-04 13:49:24 +0900

    [Silver II] Title: 결혼식, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.cc
+++ 백준/Silver/5567. 결혼식/결혼식.cc
@@ -1,34 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a, b;
-int ans = 0;
-vector<int> adj[505];
-int vis[505];
-queue<int> q;
 
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    fill(vis, vis+505, -1);
-    
+    int n, m;
     cin >> n >> m;
-    for(int i = 0; i < m; i++){
+    
+    vector<int> adj[505];
+    for (int i = 0; i < m; i++) {
+        int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
     
+    int vis[505];
+    fill(vis, vis+505, -1);
+    
+    queue<int> q;
     q.push(1);
     vis[1] = 0;
-    while(!q.empty()){
-        int cur = q.front();
-        q.pop();
-        for(auto nxt : adj[cur]){
+    
+    int ans = 0;
+    while (!q.empty()) {
+        int cur = q.front(); q.pop();
+        if (vis[cur] == 2) continue; 
+        for (auto nxt : adj[cur]) {
             if (vis[nxt] != -1) continue;
-            if (vis[cur]+1 > 2) continue;
+            vis[nxt] = vis[cur] + 1;
             q.push(nxt);
-            vis[nxt] = vis[cur]+1;
             ans++;
         }
     }
