03fe549b31ee8f328eec6abbea7aac4dbd835e25
gradeten <88281367+gradeten@users.noreply.github.com>
2025-09-10 18:44:27 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -4,26 +4,29 @@ int n, a, b;
 vector<int> adj[100005];
 int parent[100005];
 
-void dfs(int cur){
-    for(int nxt : adj[cur]){
-        if (parent[cur] == nxt) continue;
-        parent[nxt] = cur;
-        dfs(nxt);
-    }
-}
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
     cin >> n;
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n-1; i++){
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
     
-    dfs(1);
+    queue<int> q;
+    q.push(1);
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+        for(int nxt : adj[cur]){
+            if (parent[cur] == nxt) continue;
+            parent[nxt] = cur;
+            q.push(nxt);
+        }
+    }
+    
+    
     for(int i = 2; i <= n; i++){
         cout << parent[i] << '\n';
     }
