0874341afdecb2a9556f61391005d1196462a748
gradeten <88281367+gradeten@users.noreply.github.com>
2025-09-24 19:53:39 +0900

    [Gold II] Title: 문제집, Time: 28 ms, Memory: 3968 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, u, v;
+vector<int> adj[32005];
+int deg[32005];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    for(int i = 1; i <= m; i++){
+        cin >> u >> v;
+        adj[u].push_back(v);
+        deg[v]++;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        sort(adj[i].begin(), adj[i].end());
+    }
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i = 1; i <= n; i++){
+        if (deg[i] == 0) pq.push(i);
+    }
+    while(!pq.empty()){
+        int cur = pq.top(); pq.pop();
+        cout << cur << ' ';
+        for(int nxt : adj[cur]){
+            deg[nxt]--;
+            if (deg[nxt] == 0) pq.push(nxt);
+        }
+    }
+}
