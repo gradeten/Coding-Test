81d6275bd227b8d7c9e4758b063ed7a5b98ba26f
gradeten <88281367+gradeten@users.noreply.github.com>
2025-11-02 20:31:51 +0900

    [level 3] Title: 입국심사, Time: 39.75 ms, Memory: 7.16 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool solve(long long x, vector<int> times,int n){ // x 시간 안에 n명 심사 가능한지
+bool solve(long long x, vector<int> times, int n){
     long long cur = 0;
     for(int i = 0; i < times.size(); i++){
         cur += x/times[i];
+        if (cur >= n) return true;
     }
-    if (cur < n) return false;
-    else return true;
+    if (cur >= n) return true;
+    else return false;
 }
 
 long long solution(int n, vector<int> times) {
-    
     sort(times.begin(), times.end());
-    long long st = 1;
-    long long en = (long long)times[times.size()-1]*n; 
     
-    while( st < en ){
-        long long mid = (st + en ) / 2;
-        if (solve(mid, times, n)) en = mid; // 시간 덜 필요
-        else st = mid + 1; // 시간 더 필요
+    long long st = 1;
+    long long en = (long long)n*times[times.size()-1];
+    
+    while(st < en){
+        long long mid = (st + en)/2;
+        if (solve(mid, times, n)) en = mid;
+        else st = mid + 1;
     }
     
     return st;
