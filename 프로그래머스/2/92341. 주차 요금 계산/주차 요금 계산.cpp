73a2335906618ae5208551006b6fa364e626724b
gradeten <88281367+gradeten@users.noreply.github.com>
2025-11-03 21:27:58 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.07 ms, Memory: 4.08 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -4,43 +4,46 @@ using namespace std;
 vector<int> solution(vector<int> fees, vector<string> records) {
     vector<int> answer;
     map<string, string> cars;
-    map<string, int> time;
+    map<string, int> times;
     
     for(int i = 0; i < records.size(); i++){
-        string tm, car_num, status;
+        string time, car_num, status;
         istringstream iss(records[i]);
-        iss >> tm >> car_num >> status;
+        iss >> time >> car_num >> status;
         
-        if (cars.find(car_num) != cars.end()){
+        if (cars.find(car_num) != cars.end()){ 
             int st_hr = stoi(cars[car_num].substr(0,2));
             int st_mn = stoi(cars[car_num].substr(3,2));
-            int en_hr = stoi(tm.substr(0,2));
-            int en_mn = stoi(tm.substr(3,2));
+            int en_hr = stoi(time.substr(0,2));
+            int en_mn = stoi(time.substr(3,2));
             
-            time[car_num] += (en_hr*60+en_mn) - (st_hr*60+st_mn);
+            times[car_num] += ((en_hr*60+en_mn) - (st_hr*60+st_mn));
             cars.erase(car_num);
         }
-        else cars[car_num] = tm;
+        else{
+            cars[car_num] = time;
+        }
     }
     
-    for (auto c : cars){
+    for(auto c : cars){
         int st_hr = stoi(c.second.substr(0,2));
         int st_mn = stoi(c.second.substr(3,2));
         int en_hr = 23;
         int en_mn = 59;
-        int tot = (en_hr*60+en_mn) - (st_hr*60+st_mn);
-        time[c.first] += tot;
+
+        times[c.first] += ((en_hr*60+en_mn) - (st_hr*60+st_mn));
     }
     
-    
-    for (auto t : time){
+    for(auto t : times){
         if (t.second <= fees[0]){
             answer.push_back(fees[1]);
         }
         else{
-            answer.push_back
-            (fees[1] + ceil((t.second - fees[0]) / (double)fees[2]) * fees[3]);
+            answer.push_back(fees[1] + 
+                    ceil((t.second - fees[0])/(double)fees[2])*fees[3]);
         }
     }
+    
+    
     return answer;
 }
