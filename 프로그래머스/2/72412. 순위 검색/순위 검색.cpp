ab56c78a4fa29f84b2463669ab9eb20523e65183
gradeten <88281367+gradeten@users.noreply.github.com>
2025-11-03 21:42:41 +0900

    [level 2] Title: 순위 검색, Time: 319.26 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-map<string, vector<int>> m;
+map<string, vector<int>> keys;
 
 void makeKeys(int idx, string key, vector<string>& data, int score){
     if (idx == 4){
-        m[key].push_back(score);
+        keys[key].push_back(score);
         return;
     }
     makeKeys(idx+1, key + data[idx], data, score);
@@ -16,36 +15,34 @@ vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
     
     for(int i = 0; i < info.size(); i++){
-        string lang, job, career, food;
-        int score;
+        string lang, job, level, food, score;
         istringstream iss(info[i]);
-        iss >> lang >> job >> career >> food >> score;
-        
-        vector<string> data = {lang, job, career, food};
-        makeKeys(0,"", data, score);
+        iss >> lang >> job >> level >> food >> score;
+        vector<string> data = {lang, job, level, food};
+        makeKeys(0, "", data, stoi(score));
     }
     
-    for(auto& p : m){
-        sort(p.second.begin(), p.second.end());
+    for(auto& k : keys){
+        sort(k.second.begin(), k.second.end());
     }
     
     for(int i = 0; i < query.size(); i++){
-        string lang, and1, job, and2, career, and3, food;
-        int num_cond;
+        string lang, and1, job, and2, level, and3, food, score;
         istringstream iss(query[i]);
-        iss >> lang >> and1 >> job >> and2 >> career >> and3 >> food >> num_cond;
+        iss >> lang >> and1 >> job >> and2 >> level >> and3 >> food >> score;
         
-        string key = lang + job + career + food;
+        string cond = lang + job + level + food;
+        int sc = stoi(score);
         
-        int result = 0;
-        if (m.find(key) != m.end()){
-            int idx = lower_bound(m[key].begin(), m[key].end(), num_cond) - m[key].begin();
-            result = m[key].size() - idx;
+        int ans = 0;
+        if (keys.find(cond) != keys.end()){
+            int idx = lower_bound(keys[cond].begin(), keys[cond].end(), sc) 
+                - keys[cond].begin();
+            ans = keys[cond].size() - idx;
         }
         
-        answer.push_back(result);
+        answer.push_back(ans);
     }
-    
     
     return answer;
 }
